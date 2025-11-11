58716f3fb5a09f0e7e7f531e60c6d1988b5b86c2
ibrohim-afk <73987956+ibrohim-afk@users.noreply.github.com>
2025-11-11 15:57:13 +0700

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ini adalah baris penting
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
